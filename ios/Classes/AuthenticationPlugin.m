3c203da9a2289ad0dd790568066dfe7627ee65c8
Jhourlad Estrella <m@jhourlad.com>
2020-08-13 12:55:05 +0800

    Initial commit

---
+++ ios/Classes/AuthenticationPlugin.m
@@ -0,0 +1,20 @@
+#import "AuthenticationPlugin.h"
+
+@implementation AuthenticationPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"authentication"
+            binaryMessenger:[registrar messenger]];
+  AuthenticationPlugin* instance = [[AuthenticationPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
